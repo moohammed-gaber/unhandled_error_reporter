177ae1e9457754354cd1bebf6bd758e17ff37545
gaber <moohammed.gaber@gmail.com>
2022-10-30 22:21:14 -0700

    ...

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:unhandled_error_reporter/src/enums/risk_level_enum.dart';
 import 'package:unhandled_error_reporter/error_capture.dart';
 import 'package:unhandled_error_reporter/src/dtos/error_dto.dart';
