c80ff4d0582e639c359aab9be09541cc2077789b
gaber <moohammed.gaber@gmail.com>
2022-11-02 00:26:41 -0700

    ......

--- lib/unhandled_error_reporter.dart
+++ lib/unhandled_error_reporter.dart
@@ -2,6 +2,7 @@ library unhandled_error_reporter;
 
 export 'src/error_capture.dart';
 export 'src/enums/risk_level_enum.dart';
+export 'src/enums/error_from.dart';
 export 'src/interfaces/remote_monitor.dart';
 export 'src/interfaces/risk_level_determiner.dart';
 export 'src/facades/unhandled_error_facade.dart';
